4772add5e4e76fdf2755b2fee464ccefde05c730
FadzararAI <89302350+FadzararAI@users.noreply.github.com>
2024-04-30 08:02:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,49 +11,70 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
-	<link rel="stylesheet" type="text/css" href="styles.css" />
+    <link rel="stylesheet" type="text/css" href="index.css" />
 </head>
 <body>
-<div class="hero">
-<nav class="top-nav">
-	<ul class="menu-left">
-		<li><a href="#">
-			<div class="nav-logo">
-			a
-			</div>
-		</a></li>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="companies.php">Companies</a></li>
-		<li><a href="jobsearch.php">Find Jobs</a></li>
-	</ul>
-	<ul class="menu-right">
-		<li><a href="sign-in.php">Employers</a></li>
-		<li><a>|</a></li>
-		<li><a href="sign-in.php">Sign In</a></li>
-	</ul>
-</nav>
-<h1>Find The Perfect Job For You</h1>
-<form method="GET">
-	<div class="searchers">
-		<input type="text" id="jobsearch" name="jname" placeholder="Search for jobs.." title="Search a job title" required>
-		<input type="text" id="locsearch" name="jloc" placeholder="Search for locations.." title="Search a job location">
-		<button type="submit">Find Job</button>
-	</div>
-</form>
-</div>
-
-<div class="cate">
-<h1>Explore By Category</h1>
-</div>
-<?php
-  if(!isset($_GET["jloc"]) && isset($_GET["jname"])){
-    $jobname = $_GET["jname"];
-    echo "<script>document.alert('location not set')</script>";
-    
-  }
-?>
-<div class="joblists">
-	<div class="circle-container">
+	<section class="upper">
+		<!-- navigation start -->
+	    <nav class="nav">
+	      <div class="navigation">
+	        <img src="./images/hero_logo.png" alt="logo" class="logo" />
+	        <div class="links">
+	          <a href="index.php">Home</a>
+	          <a href="companies.php">Companies</a>
+	          <?php
+	          if(isset($_SESSION["type"])){
+	            if(trim($_SESSION["type"]) == 'company'){
+	              echo "<a href='jobsearch.php'>View jobs</a>";
+	              echo "<a href='add-job.php'>Post a job</a>";
+	            }else{
+	              echo "<a href='jobsearch.php'>Find Jobs</a>";
+	            }
+	          }else{
+	            echo "<a href='jobsearch.php'>Find Jobs</a>";
+	          }
+	          ?>
+	        </div>
+	      </div>
+	      <div class="profile">
+	        <?php
+	          if(!isset($_SESSION['type'])){
+	            echo "<a href='sign-in.php'>Sign In</a>";
+	          }elseif(isset($_SESSION["type"])){
+	            if(trim($_SESSION['type']) == 'company'){
+	              echo "<a href='company_profile.php'>Company Profile</a>";
+	              echo "<a href='logout.php'>Logout</a>";
+	            }elseif(trim($_SESSION['type']) == 'student'){
+	              echo "<a href='my_profile.php'>My Profile</a>";
+	              echo "<a href='logout.php'>Logout</a>";
+	            }
+	          }
+	        ?>
+	      </div>
+	    </nav>
+    <!-- navigation end -->
+    <div class="hero">
+    	<div class="search">
+    		<h1>Find The Perfect Job For You</h1>
+    		<form method="GET" action="jobsearch.php">
+    			<div class="searches">
+    				<input type="text" id="jobsearch" name="jobname" placeholder="Search for jobs.." title="Search a job title" required>
+					<select id="locsearch" name="location" id="locs">
+			            <option value="" selected>Search based by location..</option>
+			            <option value="Jakarta">Jakarta</option>
+			            <option value="Bandung">Bandung</option>
+			        	<option value="Surabaya">Surabaya</option>
+			        </select>
+					<button type="submit">Find Job</button>
+    			</div>
+    		</form>
+    	</div>
+    </div>
+	</section>
+	<section class="categories">
+		<h1>Explore By Category</h1>
+		<div class="joblists">
+			<div class="circle-container">
           <div class="circle">
             <img src="./images/icons/technology_bw.png">
           </div>
@@ -81,10 +105,11 @@
           <font>Human Resource</font>
         </div>
 </div>
-    <!-- footer start -->
+	</section>
+	<!-- footer start -->
     <footer>
       <div class="top-container">
-        <p class="titles">Internship <br />Website</p>
+        <p class="titles">InternLink</p>
         <div class="box-navigation">
           <div class="navigation">
             <p>Internship website</p>
